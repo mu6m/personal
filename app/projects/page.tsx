249fd7de42df41f7057d1849af91da56cdc59616
Your Name <you@example.com>
2024-01-23 19:06:21 +0300

    fixed project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,14 +1,38 @@
 "use client";
+import Projects from "@/components/Projects";
 import styled from "@emotion/styled";
 
 const MainHero = styled.div`
+	width: 100%;
+	max-width: 60rem;
+	min-height: 5rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	.head-text {
+		font-family: "Playfair Display", serif;
+		font-weight: 800;
+		font-style: italic;
+		padding-inline: 2rem;
+		font-size: 8rem;
+		line-height: 8rem;
+		margin-block: 2rem;
+	}
 	@media (max-width: 60rem) {
+		h1 {
+			max-width: 40rem;
+			line-height: 7rem;
+			font-size: 5rem;
+		}
 	}
 `;
 export default function Page() {
 	return (
 		<>
-			<MainHero></MainHero>
+			<MainHero>{/* <h1 className="head-text">My Projects</h1> */}</MainHero>
+			<Projects />
 		</>
 	);
 }
