8834dad555516680dff01cced94d2d8dbec73d8c
mu6m <theledraa@gmail.com>
2024-07-21 11:40:08 +0300

    adding mastadone tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
+				<link rel="me" href="https://mastodon.social/@ledraa" />
 				<link
 					rel="icon"
 					href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2280%22>🖼️</text></svg>"
